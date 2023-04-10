90bb373bb33faf71b67c40964f836b76e9afd5e1
Ji Hoon Moon <variableruby@gmail.com>
2023-04-11 02:44:10 +0800

    Codeblock changes

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,26 +1,16 @@
 window.MathJax = {
-  tex2jax: {
-    inlineMath: [
-      ["$", "$"],
-      ["\\(", "\\)"],
-    ],
-    displayMath: [
-      ["$$", "$$"],
-      ["\\[", "\\]"],
-    ],
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
   },
-  TeX: {
-    TagSide: "right",
-    TagIndent: ".8em",
-    MultLineWidth: "85%",
-    equationNumbers: {
-      autoNumber: "AMS",
-    },
-    unicode: {
-      fonts: "STIXGeneral,'Arial Unicode MS'",
-    },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
   },
-  displayAlign: "center",
-  showProcessingMessages: false,
-  messageStyle: "none",
 };
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
