fb22bffaf351a14291760343c01dd1414f503ccd
Ji Hoon Moon <variableruby@gmail.com>
2023-04-18 00:13:33 +0800

    A

--- docs/javascripts/obsidianImage.js
+++ docs/javascripts/obsidianImage.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   var images = document.querySelectorAll("img");
   for (var i = 0; i < images.length; i++) {
-    var matches = images[i].alt.match(/^\[(\d+)\]$/);
+    var matches = images[i].alt.match(/^(\d+)$/);
     if (matches) {
-      images[i].style.height = matches[1] + "px";
+      images[i].setAttribute('height', matches[1]);
+      images[i].setAttribute('width', 'auto');
     }
   }
 });
